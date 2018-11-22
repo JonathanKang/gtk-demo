9552627ac894042d0dcafc87000052b4919b9df2
Jonathan Kang <jonathankang@gnome.org>
2018-11-22 16:34:12 +0800

    Add GdApplication Class

---
+++ src/gd-application.c
@@ -0,0 +1,41 @@
+#include <gtk/gtk.h>
+
+#include "gd-application.h"
+
+struct _GdApplication
+{
+    /* <private> */
+    GtkApplication parent_instance;
+};
+
+G_DEFINE_TYPE (GdApplication, gd_application, GTK_TYPE_APPLICATION)
+
+static void
+on_activate (GdApplication *application,
+             gpointer user_data)
+{
+    GtkWidget *window;
+
+    window = gtk_application_window_new (GTK_APPLICATION (application));
+    gtk_widget_show (window);
+}
+
+static void
+gd_application_init (GdApplication *application)
+{
+    g_signal_connect (application, "activate",
+                      G_CALLBACK (on_activate), NULL);
+}
+
+static void
+gd_application_class_init (GdApplicationClass *klass)
+{
+}
+
+GtkApplication *
+gd_application_new ()
+{
+    return g_object_new (GD_TYPE_APPLICATION,
+                         "application-id", "org.gtk.demo",
+                         NULL);
+}
--- src/gd-main.c
+++ src/gd-main.c
@@ -1,14 +1,6 @@
 #include <gtk/gtk.h>
 
-static void
-on_activate (GApplication *app,
-             gpointer user_data)
-{
-    GtkWidget *window;
-
-    window = gtk_application_window_new (GTK_APPLICATION (app));
-    gtk_widget_show (window);
-}
+#include "gd-application.h"
 
 int
 main (int argc, char **argv)
@@ -16,11 +8,8 @@ main (int argc, char **argv)
     GtkApplication *app;
     int status;
 
-    app = gtk_application_new ("org.gtk.demo", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect (G_APPLICATION (app), "activate",
-                      G_CALLBACK (on_activate), NULL);
+    app = gd_application_new ();
     status = g_application_run (G_APPLICATION (app), argc, argv);
 
-    g_object_unref (app);
     return status;
 }
